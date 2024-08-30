08b1c49d243439a18bff70ceb5848eef3756d2f8
João Pedro Couto <129805937+joaopcouto@users.noreply.github.com>
2024-08-30 15:16:01 -0300

    Feat: Criada tela detox

--- src/app/modules/views/detox/detox.component.ts
+++ src/app/modules/views/detox/detox.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./detox.component.css']
 })
 export class DetoxComponent {
-
+  textTittle: string = 'What is lorem';
+  textDescription: string =
+    ' Lorem ipsum dolor sit amet consectetur adipisicing elit Voluptatum quis dolore tempora quibusdam Repudiandae autem error iusto commodi consequatur suscipit ratione beatae sequi rem natus nam vero Impedit earum similique Lorem ipsum dolor sit amet consectetur adipisicing elit Voluptatum quis dolore tempora quibusdam Repudiandae autem error iusto commodi consequatur suscipit ratione beatae sequi rem natus nam vero Impedit earum similique Lorem ipsum dolor sit amet consectetur adipisicing elit Voluptatum quis dolore tempora quibusdam Repudiandae autem error iusto commodi consequatur suscipit ratione beatae sequi rem natus nam vero Impedit earum similique';
+  cardImg: string = '../../../../assets/chas/cha-01.png';
+  cardImg2: string = '../../../../assets/chas/cha-02.png';
+  cardImg3: string = '../../../../assets/chas/cha-03.png';
+  cardImg4: string = '../../../../assets/chas/cha-04.png';
 }
