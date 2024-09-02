80f95f5a831b2d066fcfa96b4d372bf8ed803cd4
João Pedro Couto <129805937+joaopcouto@users.noreply.github.com>
2024-09-02 10:37:57 -0300

    Feat: Criada view calculadora

--- src/app/modules/views/calculadora/calculadora.component.ts
+++ src/app/modules/views/calculadora/calculadora.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-calculadora',
   templateUrl: './calculadora.component.html',
-  styleUrls: ['./calculadora.component.css']
+  styleUrls: ['./calculadora.component.css'],
 })
 export class CalculadoraComponent {
+  textButton: string = "Salvar e enviar";
 
 }
